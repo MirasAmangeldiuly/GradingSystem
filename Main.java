bc8fb3866aa0bd3ae69f377d158cfca11b2fea8c
MirasAmangeldiuly <124961658+MirasAmangeldiuly@users.noreply.github.com>
2023-02-11 22:06:26 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -14,27 +14,26 @@ public class Main {
         evaluationSystem.addCourse(course2);
 
         evaluationSystem.enrollStudentInCourse(1, 1);
-        evaluationSystem.enrollStudentInCourse(1, 2);
         evaluationSystem.enrollStudentInCourse(2, 2);
 
-        Exam exam1 = new Exam(1, "Midterm", 50, 88);
+        Exam exam1 = new Exam(1, "Midterm", 40, 88);
         Exam exam2 = new Exam(2, "Final", 50, 92);
-        Assignment assignment1 = new Assignment(1, "Homework 1", 25, 20, 20);
-        Assignment assignment2 = new Assignment(2, "Homework 2", 25, 18, 20);
+        Assignment assignment1 = new Assignment(1, "Homework 1", 10, 20, 20);
+        
 
         evaluationSystem.addEvaluationToCourse(1, exam1);
         evaluationSystem.addEvaluationToCourse(1, exam2);
         evaluationSystem.addEvaluationToCourse(1, assignment1);
-        evaluationSystem.addEvaluationToCourse(1, assignment2);
+        
 
-        Exam exam3 = new Exam(3, "Midterm", 50, 83);
+        Exam exam3 = new Exam(3, "Midterm", 40, 83);
         Exam exam4 = new Exam(4, "Final", 50, 86);
-        Assignment assignment3 = new Assignment(3, "Homework 1", 25, 18, 20);
-        Assignment assignment4 = new Assignment(4, "Homework 2", 25, 19, 20);
+        Assignment assignment3 = new Assignment(3, "Homework 1", 10, 18, 20);
+        
         evaluationSystem.addEvaluationToCourse(2, exam3);
         evaluationSystem.addEvaluationToCourse(2, exam4);
         evaluationSystem.addEvaluationToCourse(2, assignment3);
-        evaluationSystem.addEvaluationToCourse(2, assignment4);
+        
         double gpa = GPA.calculateGPA(1, evaluationSystem)/25;
         double gpa2 = GPA.calculateGPA(2, evaluationSystem)/25;
         StudentGrades studentGrades = new StudentGrades();
